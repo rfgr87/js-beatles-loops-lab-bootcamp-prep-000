4fa6613a83fc2577e6a18da3ea51e1c8d5022009
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-02-06 18:57:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,6 +25,9 @@ function johnLennonFacts(array1){
 }
 
 function iLoveTheBeatles(number){
-  var array1
-  
+  var array1;
+  do{
+    array1.push("I love the Beatles!");
+    number = number + 1
+  } while(number < 15)
 }
