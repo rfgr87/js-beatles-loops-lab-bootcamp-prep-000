015875c26e719eb4674747883dd8aa56c39876fd
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-02-06 18:59:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // add solution here
 
 function theBeatlesPlay(array1, array2){
+  var array3 = []
   for(let i = -1; i < array1.length; i++){
     var musician = array1[i];
     var instrument = array2[i];
     var string = `${musician} plays ${instrument}` 
-    var array3 = []
     array3.push(string)
   }
   return array3
